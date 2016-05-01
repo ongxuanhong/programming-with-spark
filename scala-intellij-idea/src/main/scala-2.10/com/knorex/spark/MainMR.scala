29b2b3e2cc929896316fe217394cb18508120b14
Ong Xuan Hong <ongxuanhong@gmail.com>
2016-05-01 14:18:48 +0800

    Using DF without creating temp table in SparkSQL

--- scala-intellij-idea/src/main/scala-2.10/com/knorex/spark/MainMR.scala
+++ scala-intellij-idea/src/main/scala-2.10/com/knorex/spark/MainMR.scala
@@ -6,26 +6,30 @@ import com.stratio.datasource.mongodb._
 import com.stratio.datasource.mongodb.config._
 import com.stratio.datasource.mongodb.config.MongodbConfig._
 import org.apache.spark.sql.SQLContext
+import com.mongodb.casbah.{WriteConcern => MongodbWriteConcern}
 import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.sql.functions.{udf, _}
 
 object MainMR extends App {
 
   override def main(args: Array[String]) {
 
-    val conf = new SparkConf().setAppName("Knorex Spark MapReduce Application").setMaster("local[2]")
+    val conf = new SparkConf()
+      .setAppName("Knorex Spark MapReduce Application")
+      .setMaster("local[2]")
+      .set("spark.executor.memory", "2g")
     val sc = new SparkContext(conf)
     val sqlContext = new SQLContext(sc)
 
-    val builder = MongodbConfigBuilder(Map(
+    val bdConfig = MongodbConfigBuilder(Map(
       Host -> List("localhost:27017"),
       Database -> "brand_display_analytics",
       Collection -> "brand_display_04_2016",
       SamplingRatio -> 1.0,
-      WriteConcern -> "normal"))
-    val readConfig = builder.build()
-    val mongoRDD = sqlContext.fromMongoDB(readConfig)
-    mongoRDD.registerTempTable("brand_display_04_2016")
-    sqlContext.sql("select * from brand_display_04_2016").show()
+      WriteConcern -> "normal")).build()
+
+    val bdDF = sqlContext.fromMongoDB(bdConfig)
+    bdDF.select(col("widgetId"), col("date")).show()
 
     System.out.println("OK")
   }
