8deb9679724856f7206a3cc43cfb2691dec78e4b
Ong Xuan Hong <ongxuanhong@gmail.com>
2016-05-07 16:58:16 +0800

    Remove buildMongoConfig function

--- scala-intellij-idea/src/main/scala-2.10/SparkReadParquet.scala
+++ scala-intellij-idea/src/main/scala-2.10/SparkReadParquet.scala
@@ -2,25 +2,10 @@
   * Created by hongong on 5/7/16.
   */
 
-import com.stratio.datasource.mongodb._
-import com.stratio.datasource.mongodb.config._
-import com.stratio.datasource.mongodb.config.MongodbConfig._
-import org.apache.spark.sql.{SQLContext, SaveMode}
-import com.stratio.datasource.util.Config
+import org.apache.spark.sql.SQLContext
 import org.apache.spark.{SparkConf, SparkContext}
-import org.apache.spark.sql.types._
 
 object SparkReadParquet extends App {
-
-  def buildMongoConfig(host : String, database : String, collection : String) : Config = {
-    val mongoConfig = MongodbConfigBuilder(Map(
-      Host -> List(host),
-      Database -> database,
-      Collection -> collection,
-      SamplingRatio -> 1.0,
-      WriteConcern -> "normal")).build()
-    return mongoConfig
-  }
 
   override def main(args: Array[String]) {
 
