897a2034ed47fc36940d128a9ba7c77734a54803
Ong Xuan Hong <ongxuanhong@gmail.com>
2016-05-08 12:22:20 +0800

    All about WHERE clauses

--- spark-dataframes/src/main/scala-2.10/MainExecutor.scala
+++ spark-dataframes/src/main/scala-2.10/MainExecutor.scala
@@ -23,14 +23,67 @@ object MainExecutor {
       .option("inferSchema", "true") // Automatically infer data types
       .load("db/csv/customers.csv")
 
-    // SELECT statement
+    // Let sqlContext know which table you gonna work with
     df.registerTempTable("Customers")
-    val selectColumns = sqlContext.sql("SELECT CustomerName,City FROM Customers")
-    println("SELECT by sqlContext")
-    selectColumns.show(5)
 
-    println("SELECT by DataFrames")
-    df.select("CustomerName", "City").show(5)
+    // SELECT by sqlContext
+//    sqlContext.sql("SELECT CustomerName,City FROM Customers").show(5)
+//    // SELECT by DataFrames
+//    df.select("CustomerName", "City").show(5)
 
+    // SELECT DISTINCT by sqlContext
+//    sqlContext.sql("SELECT DISTINCT City FROM Customers").show(5)
+//    // SELECT DISTINCT by DataFrames
+//    df.select("City").distinct().show(5)
+
+    // WHERE clause
+//    sqlContext.sql(
+//      """
+//        SELECT * FROM Customers
+//        WHERE Country='Mexico'
+//      """).show(5)
+//    df.where("Country='Mexico'").show(5)
+//    df.filter(col("Country") === "Mexico").show(5)
+
+    // Operators in The WHERE Clause
+//    sqlContext.sql(
+//      """
+//        SELECT * FROM Customers
+//        WHERE CustomerID < 5
+//      """).show()
+//    df.where("CustomerID < 5").show()
+//    df.filter(col("CustomerID").lt(5)).show()
+//    df.filter(col("CustomerID") < 5).show()
+
+//    sqlContext.sql(
+//      """
+//        SELECT * FROM Customers
+//        WHERE CustomerID BETWEEN 1 AND 4
+//      """).show()
+//    df.where("CustomerID BETWEEN 1 AND 4").show()
+//    df.filter(col("CustomerID").between(1, 4)).show()
+
+//    sqlContext.sql(
+//      """
+//        SELECT * FROM Customers
+//        WHERE CustomerName LIKE '%ana%'
+//      """).show(5)
+//    df.where("CustomerName LIKE '%ana%'").show()
+//    df.filter(col("CustomerName").like("%ana%")).show()
+//    sqlContext.sql(
+//      """
+//        SELECT * FROM Customers
+//        WHERE CustomerID IN (1,3,5)
+//      """).show()
+//    df.where("CustomerID IN (1,3,5)").show()
+//    df.filter(col("CustomerID").isin(List(1, 3, 5):_*)).show()
+//    sqlContext.sql(
+//      """
+//        SELECT * FROM Customers
+//        WHERE CustomerID <> 3
+//      """).show(5)
+//    df.where("CustomerID <> 3").show(5)
+//    df.filter(col("CustomerID") !== 3).show(5)
+//    df.filter(col("CustomerID").notEqual(3)).show(5)
   }
 }
