fa0271555b05e7e0b974cca79a0ad77946d86aa9
Ong Xuan Hong <ongxuanhong@gmail.com>
2016-05-08 11:26:19 +0800

    SELECT columns

--- spark-dataframes/src/main/scala-2.10/MainExecutor.scala
+++ spark-dataframes/src/main/scala-2.10/MainExecutor.scala
@@ -5,6 +5,7 @@
 import org.apache.spark._
 import org.apache.spark.SparkContext
 import org.apache.spark.sql.SQLContext
+import org.apache.spark.sql.functions._
 
 object MainExecutor {
   def main(args: Array[String]) {
@@ -14,13 +15,22 @@ object MainExecutor {
       .setMaster("local[2]")
     val sc = new SparkContext(conf)
     val sqlContext = new SQLContext(sc)
+
+    // Loading customers data
     val df = sqlContext.read
       .format("com.databricks.spark.csv")
       .option("header", "true") // Use first line of all files as header
       .option("inferSchema", "true") // Automatically infer data types
       .load("db/csv/customers.csv")
 
-    df.show(5)
+    // SELECT statement
+    df.registerTempTable("Customers")
+    val selectColumns = sqlContext.sql("SELECT CustomerName,City FROM Customers")
+    println("SELECT by sqlContext")
+    selectColumns.show(5)
+
+    println("SELECT by DataFrames")
+    df.select("CustomerName", "City").show(5)
 
   }
 }
